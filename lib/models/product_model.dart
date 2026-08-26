f601a0e746bec6c5294fb5c6901acd0de8129aab
ayman <aymanmohamed1142001@gmail.com>
2026-08-27 01:43:46 +0300

    add productmodel

---
+++ lib/models/product_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+
+class ProductModel {
+  final int id;
+  final String title ;
+  final double price;
+  final String description;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel({required this.id, required this.title, required this.price, required this.description, required this.image, required this.rating});
+
+
+  factory ProductModel.fromJson(jsonData){
+    return ProductModel(
+      rating: RatingModel.fromJson(jsonData['rating']),
+      id: jsonData['id'], title:jsonData['title'], price: jsonData['price'], description:jsonData ['description'], image:jsonData ['image']);
+  }
+
+}
+
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({required this.rate, required this.count});
+
+
+  factory RatingModel.fromJson(jsonData){
+    return RatingModel(rate:jsonData ['rate'], count:jsonData ['count']);
+  }
+
+
+
+
+}
