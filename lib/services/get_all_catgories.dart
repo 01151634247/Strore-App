0dd89e1b15dee62f59f63aa5a71fcd6d387f56df
ayman <aymanmohamed1142001@gmail.com>
2026-08-27 02:14:27 +0300

    handel exception

--- lib/services/get_all_catgories.dart
+++ lib/services/get_all_catgories.dart
@@ -8,8 +8,12 @@ class GetAllCatgories {
   Future<List<dynamic>>getAllCatgories()async{
     
      http.Response response = await http.get(Uri.parse('https://fakestoreapi.com/products/categories'));
-     List<dynamic>data=jsonDecode(response.body);
-     return data;
+     if (response.statusCode==200) {
+       List<dynamic>data=jsonDecode(response.body);
+       return data;
+     }else{
+       throw Exception('there is aproblem with satatus code ${response.statusCode}');
+     }
 
   }
 }
--- lib/services/get_all_products.dart
+++ lib/services/get_all_products.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:http/http.dart%20';
 import 'package:store_app/models/product_model.dart';
 import 'package:http/http.dart'as http;
 
@@ -8,14 +9,18 @@ class GetAllProductService {
  Future <List<ProductModel>>getAllProducts()async{
 
      http.Response response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
-     List<dynamic>data=jsonDecode(response.body);
-     
-     List<ProductModel>productmodellist=[];
-     for(int i=0; i<data.length;i++){
-      productmodellist.add(ProductModel.fromJson(data[i]));
-             
+     if(response.statusCode==200){
+       List<dynamic>data=jsonDecode(response.body);
+       
+       List<ProductModel>productmodellist=[];
+       for(int i=0; i<data.length;i++){
+        productmodellist.add(ProductModel.fromJson(data[i]));
+               
+       }
+       return productmodellist;    
+     }else{
+       throw Exception('there is aproblem with satatus code ${response.statusCode}');
      }
-     return productmodellist;    
   }
 
 }
