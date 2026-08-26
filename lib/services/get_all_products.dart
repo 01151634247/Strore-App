7dd642091c9450d7535a07bd8c7bdfc895ccb2d2
ayman <aymanmohamed1142001@gmail.com>
2026-08-27 01:44:18 +0300

    add service to get all products

---
+++ lib/services/get_all_products.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:store_app/models/product_model.dart';
+import 'package:http/http.dart'as http;
+
+class GetAllProductService {
+
+ Future <List<ProductModel>>getAllProducts()async{
+
+     http.Response response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
+     List<dynamic>data=jsonDecode(response.body);
+     
+     List<ProductModel>productmodellist=[];
+     for(int i=0; i<data.length;i++){
+      productmodellist.add(ProductModel.fromJson(data[i]));
+             
+     }
+     return productmodellist;
+
+
+
+
+    
+  }
+
+
+
+}
