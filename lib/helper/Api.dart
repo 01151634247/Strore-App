1239482adae8643f5063811dc00430656e35d210
ayman <aymanmohamed1142001@gmail.com>
2026-08-27 04:44:18 +0300

    crate post method  in Api class

--- lib/helper/Api.dart
+++ lib/helper/Api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 class Api {
  Future<dynamic>get({required String url})async{
@@ -15,6 +16,30 @@ class Api {
  }
 
 
+ Future<dynamic> post({required String url,@required dynamic body,@required String?token})async{
+
+  Map<String,String>headers={};
+  if (token!=null){
+    headers.addAll({'Authorization':'Bearer $token'});
+  }
+
+  http.Response response = await http.post(Uri.parse(url),
+  body:body,
+  headers: headers,
+);
+
+     Map<String ,dynamic> data  =jsonDecode(response.body);
+     return data;
+}
+
+/**
+ * title': 'test product',
+'price':' 13.5',
+'description': 'lorem ipsum set',
+'image': 'https://i.pravatar.cc',
+'category': 'electronic'
+ * 
+ */
 
 
 
